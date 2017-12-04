0446e3d444049dbaa39304629ae84d652bc1eb0d
getup10g <getup@10g.pl>
2017-12-04 17:43:58 +0000

    update CheckPassword

--- src/main/java/com/kodilla/stringexercises/CheckPassword.java
+++ src/main/java/com/kodilla/stringexercises/CheckPassword.java
@@ -1,4 +1,5 @@
 package com.kodilla.stringexercises;
+
 /*
 
 Napisz metodę sprawdzającą, czy dane hasło ma min. 6 znaków, min. 1 dużą
@@ -9,48 +10,53 @@ public class CheckPassword {
     public boolean checking(String password) {
         boolean isCorrect = false;
         boolean helper = false;
-        int x=0;
-                do{
-                    for (int i = 1; i <= password.length(); ++i) {
-                         if(x==0&&Character.isLowerCase(password.charAt(i))) {
-                            helper = true;
-                            x=1;
-                            break;
-                            }
-                        if (x==1&&Character.isUpperCase(password.charAt(i))) {
-                            helper=true;
-                            x=2;
-                            break;
-                            }
-                        if (x==2&&Character.isDigit(password.charAt(i))) {
-                            helper=true;
-                            x=3;
-                            break;
-                            }
-                        if (x==3) {
-                           String[] specialCharactercs ={"!","@","#","$","%","^","&","*","(",")","{","}","[","]","|",":",",","'","<",">","?",",",".","/"};
-                            for(int z =0;z<specialCharactercs.length;++z){
-                                for(int j =0;j<password.length();++j){
-                                    String character="";
-                                    character+=password.charAt(j);
-                                    if(specialCharactercs[z]==character){
-                                        helper = true;
-                                        x = 4;
-                                        isCorrect=true;
-                                        break;
-                                    }
+        int x = 0;
+        int y = 0;
+        if (password.length() >= 6) {
+            do {
+                OUTER_LOOP:
+                for (int i = 0; i < password.length(); ++i) {
+                    if (x == 0 && Character.isLowerCase(password.charAt(i))) {
+                        helper = true;
+                        x = 1;
+                    }
+                    if (x == 1 && Character.isUpperCase(password.charAt(i))) {
+                        helper = true;
+                        x = 2;
+                    }
+                    if (x == 2 && Character.isDigit(password.charAt(i))) {
+                        helper = true;
+                        x = 3;
+                    }
+                    if (x == 3) {
+                        String[] specialCharacters = {"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", "|", ":", ",", "'", "<", ">", "?", ",", ".", "/"};
+                        for (int z = 0; z < specialCharacters.length; ++z) {
+                            for (int j = 0; j < password.length(); ++j) {
+                                String character = "";
+                                character += password.charAt(j);
+                                if (specialCharacters[z].equals(character)) {
+                                    x = 4;
+                                    isCorrect = true;
+                                    break OUTER_LOOP;
                                 }
                             }
-
-                            }
                         }
-                }while(helper);
-                return isCorrect;
+                    }
+                    if (x == 4) {
+
+                    }
+                }
+                ++y;
+            } while (y < password.length());
+        }
+        return isCorrect;
     }
+
     public static void main(String[] args) {
 
         CheckPassword print = new CheckPassword();
-        System.out.println(print.checking("JavaJ3st66^^"));
+        System.out.println(print.checking("MyPasswordIs4M3^^"));
+
     }
 }
 
